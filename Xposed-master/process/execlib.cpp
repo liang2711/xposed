9b1849312dc0e6ad027a3cb381f368f5e2adfab7
liang2711 <1547567910@qq.com>
2024-02-20 14:45:26 +0800

    Create execlib.cpp

---
+++ Xposed-master/process/execlib.cpp
@@ -0,0 +1,20 @@
+#include <dlfcn.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <cstring>
+#include <ctype.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    const char* xposedLibPath=NULL;
+    if (/* condition */)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
